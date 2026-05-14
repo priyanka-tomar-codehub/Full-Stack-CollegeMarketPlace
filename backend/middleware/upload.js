6480ada7d7a62454f0d8fa099a869dda1e92422d
Priyanka Tomar <tomarpriyanka181@gmail.com>
2026-05-14 14:56:05 +0530

    ES module changes

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,6 +1,6 @@
-const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../config/cloudinary");
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
 const storage = new CloudinaryStorage({
   cloudinary,
@@ -12,4 +12,4 @@ const storage = new CloudinaryStorage({
 
 const upload = multer({ storage });
 
-module.exports = upload;
+export default upload;
